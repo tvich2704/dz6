412bf1d1c3542ccef8f30205546d76be537f0672
krasnobaev.a.a <tvich2704@gmail.com>
2022-12-18 15:32:06 +0500

    dz6

---
+++ dz41/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите с клавиатуры M чисел через пробел:");
+string[] str = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine($"{String.Join(",", str)}->{bol(str)}");
+
+int bol(string[] array){
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        int per = int.Parse(array[i]);
+        if ( per > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
---
+++ dz43/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("b1:");
+int b1 = int.Parse(Console.ReadLine());
+Console.Write("k1:");
+int k1 = int.Parse(Console.ReadLine());
+Console.Write("b2:");
+int b2 = int.Parse(Console.ReadLine());
+Console.Write("k2:");
+int k2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"b1={b1},k1={k1},b2={b2},k2={k2}->{func(b1, k1, b2, k2)}");
+
+string func(double B1, double K1, double B2, double K2){
+    double x = (B2-B1)/(K1-K2);
+    double y = K2*x+B2;
+    string answer = "(" + x.ToString() +  ";" + y.ToString() + ")";
+    return answer;
+}
+
